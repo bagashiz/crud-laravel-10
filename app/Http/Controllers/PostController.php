e3722dea86c39412a15e59bc4e24c6368fd9e13a
bagashiz <work.bitsy@simplelogin.com>
2023-03-02 22:59:08 +0700

    add create and store handler + view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use App\Models\Post;
 
 use Illuminate\View\View;
 
+use Illuminate\Http\RedirectResponse;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -22,5 +24,46 @@ class PostController extends Controller
 
         // return view with posts
         return view('posts.index', compact('posts'));
+    }
+
+    /** 
+     * create
+     * 
+     * @return View
+     */
+    public function create(): View
+    {
+        // return view
+        return view('posts.create');
+    }
+
+    /**
+     * store
+     * 
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        // validate form
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10',
+        ]);
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        // create post
+        Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        // redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data berhasil disimpan!']);
     }
 }
