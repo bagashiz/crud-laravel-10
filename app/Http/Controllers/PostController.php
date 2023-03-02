85f1ce91786d9e626b06bbfd3df86975abf6b33c
bagashiz <work.bitsy@simplelogin.com>
2023-03-02 22:46:52 +0700

    add post controller and view

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+use Illuminate\View\View;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * index
+     * 
+     * @return View
+     */
+    public function index(): View
+    {
+        // get posts
+        $posts = Post::latest()->paginate(5);
+
+        // return view with posts
+        return view('posts.index', compact('posts'));
+    }
+}
